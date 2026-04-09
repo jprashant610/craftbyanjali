816c4150baf938819342b5b0a0fca6a6e8a4bebd
Prashant Vilas Jadhav <jprashant610@gmail.com>
2026-04-09 06:20:01 +0000

    Added rangoli gallery website

---
+++ script.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll(".gallery img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.getElementById("close");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+};
+
+modal.onclick = () => {
+    modal.style.display = "none";
+};
